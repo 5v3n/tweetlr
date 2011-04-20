96a17ee6697b8e42c387de11a3ed4de2f41f13cf
Sven Kräuter | 5v3n <sven.kraeuter@gmx.net>
2011-04-20 23:42:42 +0200

    alpha version

--- Rakefile
+++ Rakefile
@@ -6,14 +6,14 @@ require 'rake/rdoctask'
 require 'rake/testtask'
 
 spec = Gem::Specification.new do |s|
-  s.name = 'newproject'
+  s.name = 'tweetlr'
   s.version = '0.0.1'
   s.has_rdoc = true
   s.extra_rdoc_files = ['README.md', 'LICENSE']
-  s.summary = 'a new project'
+  s.summary = 'an unholy alliance between twitter and tumblr'
   s.description = s.summary
   s.author = 'Sven Kraeuter'
-  s.email = 'mail@5v3n.com'
+  s.email = 'mail@svenkraeuter.com'
   s.homepage = "http://github.com/5v3n/#{s.name}"
   # s.executables = ['your_executable_here']
   s.files = %w(LICENSE README.md Rakefile) + Dir.glob("{bin,lib,spec}/**/*")
@@ -31,7 +31,7 @@ Rake::RDocTask.new do |rdoc|
   files =['README.md', 'LICENSE', 'lib/**/*.rb']
   rdoc.rdoc_files.add(files)
   rdoc.main = "README.md" # page to start on
-  rdoc.title = "newproject Docs" # <--- enter name manually!
+  rdoc.title = "tweetlr Docs" # <--- enter name manually!
   rdoc.rdoc_dir = 'doc/rdoc' # rdoc output folder
   rdoc.options << '--line-numbers'
 end
---
+++ spec/tweetlr_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../lib/tweetlr.rb'
+require 'net/http'
+
+describe Tweetlr do
+  before :each do
+    @credentials = {:email => 'MAIL', :password => 'PW'}
+    @cookie = "tmgioct=as3u4KJr9COyJA9j4nwr6ZAn"
+    @searchterm = 'fail'
+    @twitter_response = {"from_user_id_str"=>"1915714", "profile_image_url"=>"http://a0.twimg.com/profile_images/386000279/2_normal.jpg", "created_at"=>"Sun, 17 Apr 2011 16:48:42 +0000", "from_user"=>"polarity", "id_str"=>"59659561224765440", "metadata"=>{"result_type"=>"recent"}, "to_user_id"=>nil, "text"=>"Rigaer #wirsounterwegs   @ Augenarzt Dr. Lierow http://instagr.am/p/DQWAj/", "id"=>59659561224765440, "from_user_id"=>1915714, "geo"=>{"type"=>"Point", "coordinates"=>[52.5182, 13.454]}, "iso_language_code"=>"de", "place"=>{"id"=>"3078869807f9dd36", "type"=>"city", "full_name"=>"Berlin, Berlin"}, "to_user_id_str"=>nil, "source"=>"&lt;a href=&quot;http://instagr.am&quot; rel=&quot;nofollow&quot;&gt;instagram&lt;/a&gt;"}
+    @links = {
+      :instagram => "http://instagr.am/p/DQWAj/",
+      :twitpic => "http://twitpic.com/449o2x",
+      :yfrog => "http://yfrog.com/h4vlfp",
+      :picplz => "http://picplz.com/2hWv"
+      }
+    @pic_regexp = /(.*?)\.(jpg|jpeg|png|gif)$|(http:\/\/twitpic.com\/show\/full\/449o2x)/i #naive approach - but should do the trick.
+    @tweetlr = Tweetlr.new @credentials[:email], @credentials[:password], @cookie, nil, @searchterm
+  end
+  it "should post to tumblr" do
+    tweetlr = @tweetlr
+    response = tweetlr.post_to_tumblr({:type => 'regular', :title => "test post", :body => "body of the test post...", :email => @credentials[:email], :password => @credentials[:password]})
+    response.response.should be_kind_of Net::HTTPCreated
+  end
+  it "should search twitter for a given term" do
+    tweetlr = @tweetlr
+    response = tweetlr.search_twitter
+    tweets = response.parsed_response['results']
+    tweets.should be
+    tweets.should_not be_empty
+  end
+  describe "image url processing" do
+    it "should find a picture's url from the instagram short url" do
+      check_pic_url_extraction :instagram
+    end
+    it "should find a picture's url from the picplz short url" do
+      check_pic_url_extraction :picplz
+    end
+    it "should find a picture's url from the yfrog short url" do
+      check_pic_url_extraction :yfrog
+    end
+    it "should find a picture's url from the twitpic short url" do
+      check_pic_url_extraction :twitpic
+    end
+  end
+  describe "tweet api response processing" do
+    it "should extract links" do
+      tweetlr = @tweetlr
+      link = tweetlr.extract_link @twitter_response
+      link.should == @links[:instagram]
+      link = tweetlr.extract_link @twitter_response.merge 'text' => @links[:instagram].chop #check if it works w/o the trailing slash
+      link.should == @links[:instagram].chop
+    end
+  end
+  def check_pic_url_extraction(service)
+    tweetlr = @tweetlr
+    image_url = tweetlr.send "image_url_#{service}".to_sym, @links[service]
+    image_url.should =~ @pic_regexp 
+  end
+  
+end
+
