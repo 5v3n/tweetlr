3d4907cbd0fe8a8832777cac46dbf3754a782f37
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-04-27 21:32:27 +0200

    version bump

--- lib/tweetlr.rb
+++ lib/tweetlr.rb
@@ -9,7 +9,7 @@ require 'uri'
 
 class Tweetlr
 
-  VERSION = '0.1.13'
+  VERSION = '0.1.14'
   
   API_ENDPOINT_TWITTER = 'http://search.twitter.com/search.json'
   API_ENDPOINT_TUMBLR = 'http://www.tumblr.com'
