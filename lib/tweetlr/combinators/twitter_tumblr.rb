bb283762c341c87caa37e7c24452481a940fd6be
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2013-04-28 21:57:51 +0200

    cleaning up a little more...

--- lib/tweetlr/combinators/twitter_tumblr.rb
+++ lib/tweetlr/combinators/twitter_tumblr.rb
@@ -25,30 +25,36 @@ module Tweetlr::Combinators
     #generate the data for a tumblr photo entry by parsing a tweet
     def self.generate_photo_post_from_tweet(tweet, options = {})
       log.debug "#{self}.generate_photo_post_from_tweet with options: #{options.inspect}"
-      tumblr_post = nil
+      process_options_and_tweet options, tweet
+    end
+private 
+    def self.process_options_and_tweet(options, tweet)
       whitelist = options[:whitelist]
       whitelist.each {|entry| entry.downcase!} if (whitelist && whitelist.size != 0)
       if !Tweetlr::Processors::Twitter::retweet? tweet['text']
         log.debug "tweet: #{tweet}"
-        tumblr_post = {}
-        tumblr_post[:tumblr_blog_hostname] = options[:tumblr_blog_hostname] || options[:group]
-        tumblr_post[:type] = 'photo'
-        tumblr_post[:date] = tweet['created_at']
-        tumblr_post[:source] = extract_image_url tweet, options[:embedly_key]
-        user = tweet['from_user']
-        tumblr_post[:tags] = user
-        tweet_id = tweet['id']
-        if !whitelist || whitelist.size == 0 || whitelist.member?(user.downcase)
-          state = 'published'
-        else
-          state = 'draft'
-        end
-        tumblr_post[:state] = state
-        shouts = " #{@shouts}" if @shouts
-        tumblr_post[:caption] = %?<a href="http://twitter.com/#{user}/statuses/#{tweet_id}" alt="tweet">@#{user}</a>#{shouts}: #{tweet['text']}? 
-        #TODO make the caption a bigger matter of yml/ general configuration
+        tumblr_post = prepare_tumblr_post options, tweet, whitelist
       end
-      tumblr_post
+    end
+    def self.prepare_tumblr_post(options, tweet, whitelist)
+      tumblr_post = {}
+      tumblr_post[:tumblr_blog_hostname] = options[:tumblr_blog_hostname] || options[:group]
+      tumblr_post[:type] = 'photo'
+      tumblr_post[:date] = tweet['created_at']
+      tumblr_post[:source] = extract_image_url tweet, options[:embedly_key]
+      user = tweet['from_user']
+      tumblr_post[:tags] = user
+      tweet_id = tweet['id']
+      if !whitelist || whitelist.size == 0 || whitelist.member?(user.downcase)
+        state = 'published'
+      else
+        state = 'draft'
+      end
+      tumblr_post[:state] = state
+      shouts = " #{@shouts}" if @shouts
+      tumblr_post[:caption] = %?<a href="http://twitter.com/#{user}/statuses/#{tweet_id}" alt="tweet">@#{user}</a>#{shouts}: #{tweet['text']}? 
+      #TODO make the caption a bigger matter of yml/ general configuration
+      tumblr_post  
     end
   end
 end
